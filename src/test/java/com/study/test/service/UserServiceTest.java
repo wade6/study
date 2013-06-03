b7808c9b515e9b21df5e365f246ec1ce026a99ba
hm <wade6hm@gmail.com>
2013-06-03 23:03:02 +0800

    unittest

---
+++ src/test/java/com/study/test/service/UserServiceTest.java
@@ -0,0 +1,35 @@
+/**
+ * Project: study
+ * File Created at 2013-6-2下午10:03:44
+ */
+package com.study.test.service;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import com.alibaba.webx.study.app1.biz.service.UserService;
+
+/**
+ * 
+ * @author zhaoyuanli
+ * 2013-6-2下午10:03:44
+ */
+@RunWith(JUnit4.class)
+public class UserServiceTest {
+    
+    UserService userService;
+    
+    @Test
+    public void addUserTest(){
+        
+    }
+    
+    @Test
+    @Ignore
+    public void deleteUserTest(){
+        
+    }
+
+}
